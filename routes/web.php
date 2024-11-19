28cb87c0495a114deb199b458f418ca0ccee1a24
jfbricenof81 <julianbricenoflorezsena@gmail.com>
2024-11-19 11:41:08 -0500

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::resource('productos', ProductosController::class)
+    ->only([‘index’, 'store'])
+    ->middleware(['auth', 'verified']);
+    
 require __DIR__.'/auth.php';
