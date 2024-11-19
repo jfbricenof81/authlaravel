a88398df25db72b7d94ec6d7387741caaa194f10
jfbricenof81 <julianbricenoflorezsena@gmail.com>
2024-11-19 11:47:37 -0500

    ok rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductosController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,8 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::resource('productos', ProductosController::class)
-    ->only([‘index’, 'store'])
+    ->only(['index', 'store'])
     ->middleware(['auth', 'verified']);
-    
+
 require __DIR__.'/auth.php';
